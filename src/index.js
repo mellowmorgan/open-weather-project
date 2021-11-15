3036c5a449a08c04ee1d20ab4f7fab4846d638b4
mellowmorgan <morgan.waites@gmail.com>
2021-11-15 10:36:16 -0800

    Add first api request show temp
    Co-Authored-by: Jacob Wilson <jacobleeeugenewilson@gmail.com></jacobleeeugenewilson>

--- src/index.js
+++ src/index.js
@@ -1,4 +1,25 @@
 import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import './css/styles.css';
+
+
+$("#b").click(function(){
+  
+  let city=$("#city-input").val();
+
+  let request = new XMLHttpRequest();
+  const url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=3f7948f210798d69c370707542b10744`;
+  request.onreadystatechange = function(){
+    if(this.readyState===4 && this.status ===200){
+      const response = JSON.parse(this.responseText);
+      getElements(response);
+    }
+  };
+  request.open("GET", url, true);
+  request.send();
+  function getElements(response){
+    $('.showTemp').text(`The temperature in ${response.main.temp} degree.`);
+
+  }
+
+});
