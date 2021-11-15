93c608b0269351a59ac8c5f9935d43582b582a39
mellowmorgan <morgan.waites@gmail.com>
2021-11-15 11:07:31 -0800

    add other weather deets
    Co-Authored-by: Jacob Wilson <jacobleeeugenewilson@gmail.com></jacobleeeugenewilson>

--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 
+
+function convertToFah(temp){
+  return (parseInt(temp)-273.15)*(9/5)+32;
+}
+
 $("#b").click(function(){
   
   let city=$("#city-input").val();
@@ -18,8 +23,15 @@ $("#b").click(function(){
   request.open("GET", url, true);
   request.send();
   function getElements(response){
-    $('.showTemp').text(`The temperature in ${response.main.temp} degree.`);
+    $('.showTemp').text(`The temperature in ${city} is ${response.main.temp} Kelvin degrees.`);
+    const fahTemp = (response.main.temp-273.15)*(9/5)+32;
+    $('.showTempFahrenheit').text(`The temperature in ${city} is ${fahTemp} Fahrenheit degrees.`);
+    $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%.`);
+    $('.showTempMinFahrenheit').text(`The temperature high in ${city} is ${convertToFah(response.main.temp_max)} Fahrenheit degrees.`);
+    $('.showTempMaxFahrenheit').text(`The temperature low in ${city} is ${convertToFah(response.main.temp_min)} Fahrenheit degrees.`);
 
+    $('.showWindSpeed').text(`The wind speed in ${city} is ${response.wind.speed} mph.`);
+    $('.showClouds').text(`The cloudiness in ${city} is ${response.clouds.all}%.`);
   }
 
 });
